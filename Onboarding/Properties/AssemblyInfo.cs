ca261415907a0ec4fb1538ef82068e559c96dd27
Chen <crs19990320@gmail.com>
2024-03-06 19:06:14 +1000

    init project

--- Onboarding/Properties/AssemblyInfo.cs
+++ Onboarding/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于 typelib 的 ID
-[assembly: Guid("3eee0203-2025-49f9-b506-9eb35d78910b")]
+[assembly: Guid("88c2347b-c9a1-4e8d-9738-c8f69ef310ec")]
 
 // 程序集的版本信息由下列四个值组成:
 //
