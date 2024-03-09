8bebae3ffca3e51c0bfc5c60256e2832a7472f55
janjesdaporn <jesdaporn.ch@gmail.com>
2024-03-10 02:25:01 +1100

    Update the full data model

--- Onboarding/Models/Model_sunpro.Designer.cs
+++ Onboarding/Models/Model_sunpro.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Monash\FIT5120\Onboarding\Models\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Monash\FIT5120\Onboarding\Models\Model_sunpro.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
