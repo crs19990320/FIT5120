eff39ae375a2c8c80ffec6fc471fccd7f546fcd4
Qihang-W <qwan0115@student.monash.edu>
2024-03-23 21:09:52 +1100

    Qihang push 2

---
+++ MainProject/App_Data/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table] (
+    [Id]          INT          NOT NULL,
+    [CardName]   VARCHAR (50) NOT NULL,
+    [Description] VARCHAR (50) NULL,
+    [ImagePath]   VARCHAR (50) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
