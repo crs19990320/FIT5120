1fd9c312ae95f601ffd2c7c7d30d51f5a20e3cf6
Chen <crs19990320@gmail.com>
2024-03-27 18:55:04 +1000

    JsonString

---
+++ MainProject/Controllers/QuestionsAPIController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MainProject.Models;
+
+namespace MainProject.Controllers
+{
+    public class QuestionsAPIController : Controller
+    {
+        private Database1Entities db = new Database1Entities();
+
+        // GET: QuestionsAPI
+        public ActionResult Index()
+        {
+            var questions = db.QuestionTable1.ToList();
+            return View(questions);
+        }
+
+        [HttpGet]
+        public ActionResult GetRandomData()
+        {
+
+            var randomData = db.QuestionTable1 
+                                .OrderBy(r => Guid.NewGuid())
+                                .Take(10) 
+                                .ToList();
+
+            return Json(randomData, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
