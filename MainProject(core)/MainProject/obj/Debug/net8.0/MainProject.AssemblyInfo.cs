ebb64ed4eb4fa7c645cea118082bdb50678d3efc
Chen <crs19990320@gmail.com>
2024-04-11 21:13:48 +1000

    not

--- MainProject(core)/MainProject/obj/Debug/net8.0/MainProject.AssemblyInfo.cs
+++ MainProject(core)/MainProject/obj/Debug/net8.0/MainProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MainProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a366fc6e3c812e8a446e8a6ac8ca4f743710894")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc350697dda93dc8074efdb4870b810ae4de0396")]
 [assembly: System.Reflection.AssemblyProductAttribute("MainProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MainProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
