d504f258eb679b8d5970cbadb88cc0c387a8b218
Chen <crs19990320@gmail.com>
2024-04-17 22:41:50 +1000

    game

--- MainProject(core)/MainProject/obj/Release/net8.0/MainProject.AssemblyInfo.cs
+++ MainProject(core)/MainProject/obj/Release/net8.0/MainProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MainProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a366fc6e3c812e8a446e8a6ac8ca4f743710894")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9ad5cfaa8e0ef69fb36be4dd4cc41a5711df5e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MainProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MainProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
