ce65e23c3f1039fda46df87829305f117ed4f611
Chen <crs19990320@gmail.com>
2024-04-21 17:42:19 +1000

    tutorial

--- MainProject/obj/Release/Package/PackageTmp/App_Data/dbo.Climate.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Climate]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [state] VARCHAR(50) NULL, 
-    [state_lat] FLOAT NULL, 
-    [state_long] FLOAT NULL, 
-    [temp_change] FLOAT NULL, 
-    [avr_rainfall] FLOAT NULL
-)
