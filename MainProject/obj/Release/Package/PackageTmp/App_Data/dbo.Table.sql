ab43fb5dd77b834b1eaeee8bdd2978e66d708c7d
Chen <crs19990320@gmail.com>
2024-04-21 17:42:19 +1000

    tutorial

--- MainProject/obj/Release/Package/PackageTmp/App_Data/dbo.Table.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[QuestionTable] (
-    [Id]          INT          NOT NULL,
-    [Question]    VARCHAR (MAX) NOT NULL,
-    [WaterCredit] INT NULL,
-    [CarbonCredit]   INT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
