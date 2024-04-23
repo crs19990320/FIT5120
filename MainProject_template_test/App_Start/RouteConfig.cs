4dade5e5ff91f644db5e94cfccdf0d9fb494860c
Chen <crs19990320@gmail.com>
2024-04-23 17:58:04 +1000

    route

--- MainProject_template_test/App_Start/RouteConfig.cs
+++ MainProject_template_test/App_Start/RouteConfig.cs
@@ -16,11 +16,8 @@ namespace MainProject_template_test
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-<<<<<<< HEAD
+
                 defaults: new { controller = "Home", action = "HomePage", id = UrlParameter.Optional }
-=======
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
->>>>>>> 7b1aa10c07feb9aacaa0022fcf2e6642d2c4d6f5
             );
         }
     }
