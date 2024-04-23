416cc5a6b01820dd2bc277ac1603e65843d56ff8
Chen <crs19990320@gmail.com>
2024-04-23 17:34:25 +1000

    route

---
+++ MainProject_template_test/obj/Release/Package/PackageTmp/App_Data/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[QuestionTable] (
+    [Id]          INT          NOT NULL,
+    [Question]    VARCHAR (MAX) NOT NULL,
+    [WaterCredit] INT NULL,
+    [CarbonCredit]   INT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ MainProject_template_test/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MainProject_template_test.MvcApplication" Language="C#" %>
