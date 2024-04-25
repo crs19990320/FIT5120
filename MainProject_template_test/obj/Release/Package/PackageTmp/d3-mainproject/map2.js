7cae97d2651b64f9a0833b709301789e15572728
Chen <crs19990320@gmail.com>
2024-04-25 11:16:06 +1000

    content

---
+++ MainProject_template_test/obj/Release/Package/PackageTmp/d3-mainproject/map2.js
@@ -0,0 +1,9 @@
+// map2.js
+import { setupCurrentMapState, clearCurrentMapState } from './currentMapState.js';
+
+// Call one of the setup functions on page load if needed
+window.onload = () => {
+  setupCurrentMapState();
+};
+
+
