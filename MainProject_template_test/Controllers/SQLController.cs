3fa36a33ba0fb45122365d3a6a97393f7717abf3
Chen <crs19990320@gmail.com>
2024-05-01 16:43:17 +1000

    SQL

---
+++ MainProject_template_test/Controllers/SQLController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using MainProject_template_test.Models;
+
+namespace MainProject_template_test.Controllers
+{
+
+    public class SQLController : Controller
+    {
+        private MainprojectTP28Entities1 db = new MainprojectTP28Entities1();
+
+        public ActionResult testTable()
+        {
+            var logins = db.testTable.ToList(); 
+            return View(logins);
+        }
+    }
+}
