c0715123559ca63d7dcd52fa5a40cbe0c13cb908
Qihang-W <qwan0115@student.monash.edu>
2024-05-10 17:18:27 +1000

    complete task 2

--- MainProject_template_test/Controllers/HomeController.cs
+++ MainProject_template_test/Controllers/HomeController.cs
@@ -308,7 +308,10 @@ namespace MainProject_template_test.Controllers
 
             return View(model);
         }
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
         public ActionResult Leaderboard()
         {
             return View();
@@ -338,10 +341,13 @@ namespace MainProject_template_test.Controllers
         //    return Json(isCorrect);
         //}
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 >>>>>>> Stashed changes
 >>>>>>> 72c0b405305ca5f45af4fd5fcc0c9606cf521f46
 =======
 >>>>>>> Stashed changes
 >>>>>>> 6d9a9e1b51ecca322266f354924a972521d37607
+=======
+>>>>>>> Stashed changes
     }
 }
