a5ee65ec1aa254258a9bcd9f4fb503da7fccfa68
Qihang-W <qwan0115@student.monash.edu>
2024-05-12 17:34:11 +1000

    finish quest 1

--- MainProject_template_test/quest3/quest_test.js
+++ MainProject_template_test/quest3/quest_test.js
@@ -3,8 +3,8 @@ function showQuestion(iconId) {
     document.getElementById('question-content').style.display = 'block';
 
     // Set the same background image for the image container
-    var imageContainer = document.getElementById('quest3ImageContainer');
-    imageContainer.style.backgroundImage = 'url("../../Media/quests_bg.png")'; // Example path to common background image
+    var imageContainer = document.getElementById('imageContainer');
+    imageContainer.style.backgroundImage = 'url("task3/Interactive quests - bg.png")'; // Example path to common background image
 
     var questionText = '';
     var answers = [];
@@ -17,7 +17,7 @@ function showQuestion(iconId) {
             answers = ['Increased forestation in area', 'Higher levels of greenhouse gases in the atmosphere', ' More frequent animals in forest nearby',
             'Increased rainfall in the coastal regions'];
             correctAnswer = 'Higher levels of greenhouse gases in the atmosphere';
-            iconSrc = '../../Media/element1.png';
+            iconSrc = 'task3/element1.png';
             hint = 'Hint for question 1'; // Set the hint
             break;
         case 'icon2':
@@ -25,14 +25,14 @@ function showQuestion(iconId) {
             answers = ['Trees can blow away bad smells', 'Trees can help keep the air clean by taking in a gas called carbon dioxide', 
             'Trees can make clouds and bring rain','Trees can make the air warmer'];
             correctAnswer = 'Trees can help keep the air clean by taking in a gas called carbon dioxide';
-            iconSrc = '../../Media/element2_origin.png';
+            iconSrc = 'task3/cropped/element2.png';
             hint = '';
             break;
         case 'icon3':
             questionText = 'What gas do humans produce that causes the Earth to warm up?';
-            answers = ['Oxygen' + '\n', 'Hydrogen', 'Carbon dioxide', 'Helium'];
+            answers = ['Oxygen', 'Hydrogen', 'Carbon dioxide','Helium'];
             correctAnswer = 'Carbon dioxide';
-            iconSrc = '../../Media/element3_origin.png';
+            iconSrc = 'task3/cropped/element3.png';
             hint = '';
             break;
         case 'icon4':
@@ -40,7 +40,7 @@ function showQuestion(iconId) {
             answers = ['Recycling can make new toys and games', 'Recycling can help make the air cleaner by not making as much trash',
              'Recycling can help save water and energy','Recycling can make the ground shake'];
             correctAnswer = 'Recycling can help make the air cleaner by not making as much trash';
-            iconSrc = '../../Media/element4.png';
+            iconSrc = 'task3/element4.png';
             hint = '';
             break;
         case 'icon5':
@@ -48,7 +48,7 @@ function showQuestion(iconId) {
             answers = ['Disposable plates and plastic cutlery', 'Reusable plates and utensils', 
             'Paper napkins for everything','Individually wrapped snacks'];
             correctAnswer = 'Reusable plates and utensils';
-            iconSrc = '../../Media/element5.png';
+            iconSrc = 'task3/element5.png';
             hint = '';
             break;
         case 'icon6':
@@ -56,7 +56,7 @@ function showQuestion(iconId) {
             answers = ['Locally grown vegetables', 'Clothes made of plastic', 'Imported fruit flown from overseas',
             'Packaged snacks with plastic wrapping'];
             correctAnswer = 'Locally grown vegetables';
-            iconSrc = '../../Media/element6.png';
+            iconSrc = 'task3/element6.png';
             hint = '';
             break;
         case 'icon7':
@@ -64,7 +64,7 @@ function showQuestion(iconId) {
             answers = ['Because it helps flowers grow', 'Because it can make the air dirty',
              'Because it doesn’t use up things like coal and oil','Because it makes too much noise'];
             correctAnswer = 'Because it doesn’t use up things like coal and oil ';
-            iconSrc = '../../Media/element7.png';
+            iconSrc = 'task3/element7.png';
             hint = '';
             break;
         case 'icon8':
@@ -72,7 +72,7 @@ function showQuestion(iconId) {
             answers = ['Pens that are thrown away after one use', 'Notebooks made from recycled paper', 
             'Plastic folders that break easily', 'Glitter and plastic decorations'];
             correctAnswer = 'Notebooks made from recycled paper';
-            iconSrc = '../../Media/element8.png';
+            iconSrc = 'task3/element8.png';
             hint = '';
             break;
         case 'icon9':
@@ -80,7 +80,7 @@ function showQuestion(iconId) {
             answers = ['Take shorter showers instead of long baths', 'Keep the tap running while brushing teeth', 
             'Fill the bathtub to the brim','Water her garden with a hose every day'];
             correctAnswer = 'Take shorter showers instead of long baths';
-            iconSrc = '../../Media/element9.png';
+            iconSrc = 'task3/element9.png';
             hint = '';
             break;
         case 'icon10':
@@ -88,7 +88,7 @@ function showQuestion(iconId) {
             answers = ['Turn off lights and electronics when not in use', 'Leave all devices plugged in all day', 
             'Keep the air conditioning on high','Use an electric heater all night'];
             correctAnswer = 'Turn off lights and electronics when not in use';
-            iconSrc = '../../Media/element10.png';
+            iconSrc = 'task3/element10.png';
             hint = '';
             break;          
             
@@ -99,21 +99,21 @@ function showQuestion(iconId) {
     var iconImg = document.createElement('img');
     iconImg.src = iconSrc;
     iconImg.classList.add('icon');
-    iconImg.style.width = '600px'; // Example inline style for width
+    iconImg.style.width = '900px'; // Example inline style for width
     iconImg.style.height = 'auto'; // Example inline style for height
     iconImg.style.position = 'absolute'; // Set position to absolute
-    iconImg.style.left = '-90px'; // Example inline style for left position
-    iconImg.style.top = '150px'; // Example inline style for top position
+    iconImg.style.left = '20px'; // Example inline style for left position
+    iconImg.style.top = '200px'; // Example inline style for top position
     iconImg.style.pointerEvents = 'none'; // Disable pointer events
 
     // Create question text element
-    var questionTextElement = document.createElement('h3');
+    var questionTextElement = document.createElement('p');
     questionTextElement.textContent = questionText;
-    questionTextElement.style.fontSize = '23px'; // Example inline style for font size
+    questionTextElement.style.fontSize = '30px'; // Example inline style for font size
     questionTextElement.style.textAlign = 'left'; // Example inline style for text alignment
     questionTextElement.style.position = 'relative'; // Example inline style for positioning
-    questionTextElement.style.top = '-60px'; // Example inline style for top position
-    questionTextElement.style.left = '0px'; // Example inline style for left position
+    questionTextElement.style.top = '20px'; // Example inline style for top position
+    questionTextElement.style.left = '80px'; // Example inline style for left position
 
 
 
@@ -128,11 +128,11 @@ function showQuestion(iconId) {
         label.appendChild(input);
         label.appendChild(document.createTextNode(answer));
         label.appendChild(document.createElement('br'));
-        label.style.fontSize = '23px'; // Example inline style for font size
+        label.style.fontSize = '24px'; // Example inline style for font size
         label.style.textAlign = 'left'; // Example inline style for text alignment
         label.style.position = 'relative'; // Example inline style for positioning
-        label.style.top = '-60px'; // Example inline style for top position
-        label.style.left = '0px'; // Example inline style for left position
+        label.style.top = '20px'; // Example inline style for top position
+        label.style.left = '80px'; // Example inline style for left position
         answerOptionsElement.appendChild(label);
     });
 
@@ -155,15 +155,15 @@ function showQuestion(iconId) {
 
     // Create an img element for the summit image
     var summitButtonImage = document.createElement('img');
-    summitButtonImage.src = '../../Media/submit_button.png';
+    summitButtonImage.src = 'task3/submit_button.png'; // Replace 'task3/submit_button.png' with the actual path to your summit image
     summitButtonImage.alt = 'Submit Answer';
 
     // Adjust size and position of the summit button image
     summitButtonImage.style.width = '400px'; // Set the width of the button
     summitButtonImage.style.height = '200px'; // Set the height of the button
     summitButtonImage.style.position = 'absolute'; // Set the position to absolute
-    summitButtonImage.style.top = '450px'; // Set the distance from the top of the container
-    summitButtonImage.style.left = '520px'; // Set the distance from the left of the container
+    summitButtonImage.style.top = '670px'; // Set the distance from the top of the container
+    summitButtonImage.style.left = '920px'; // Set the distance from the left of the container
 
     // Attach click event listener to summit button image
     summitButtonImage.addEventListener('click', function() {
@@ -173,17 +173,17 @@ function showQuestion(iconId) {
 
     // Create back button image dynamically
     var backButtonImage = document.createElement('img');
-    backButtonImage.src = '../../Media/close_tab.png'; // Set the source path of your back button image
+    backButtonImage.src = 'task3/close_tab.png'; // Set the source path of your back button image
     backButtonImage.alt = 'Back';
     backButtonImage.id = 'backButtonImage';
     backButtonImage.addEventListener('click', returnToMain); // Attach click event listener to returnToMain function
 
     // Adjust size and position of the back button
-    backButtonImage.style.width = '80px'; // Set the width of the button
+    backButtonImage.style.width = '100px'; // Set the width of the button
     backButtonImage.style.height = '100px'; // Set the height of the button
     backButtonImage.style.position = 'absolute'; // Set the position to absolute
-    backButtonImage.style.top = '30px'; // Set the distance from the top of the container
-    backButtonImage.style.left = '890px'; // Set the distance from the left of the container
+    backButtonImage.style.top = '60px'; // Set the distance from the top of the container
+    backButtonImage.style.left = '1420px'; // Set the distance from the left of the container
 
     // Create a container for buttons
     var buttonContainer = document.createElement('div');
@@ -232,9 +232,16 @@ function answerQuestion(correctAnswer) {
 
 function returnToMain() {
     // Reset background image to the original one
-    var imageContainer = document.getElementById('quest3ImageContainer');
-    imageContainer.style.backgroundImage = 'url("../../Media/quests_background.png")'; // Replace background image
+    var imageContainer = document.getElementById('imageContainer');
+    imageContainer.style.backgroundImage = 'url("task3/Interactive quests - background.png")'; // Replace background image
 
     document.getElementById('question-content').style.display = 'none';
     document.getElementById('image-content').style.display = 'block';
 }
+
+
+
+
+
+
+
